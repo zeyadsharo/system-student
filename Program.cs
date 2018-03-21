7bcde8b28e3c6c3a462f1496b439b9de8145cb0a
zeyad sharo <zeyadsharo85@gmail.com>
2018-03-21 11:12:37 +0300

    Intial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_student1
+{
+    class student
+    {
+
+
+
+
+
+
+
+
+
+
+
+    }//end the class student 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
